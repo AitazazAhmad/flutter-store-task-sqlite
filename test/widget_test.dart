874790514c2a21dd139ebc861ee0d1aa5db01cd0
AitazazAhmad <aitazazahmad007@gamil.com>
2025-09-03 21:29:12 +0500

    change App name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_login_form/login_page.dart';
+import 'package:store_app/login_page.dart';
 
 void main() {
   testWidgets('Login form UI elements are present', (
